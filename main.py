f3f37fbeeacd5dcdd6a76cb65580e25182b23f67
Raymond Haack <rhaack@subseven.net>
2025-07-29 08:18:33 -0400

    chore(logging): persist incoming webhook data for review

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+import json
 
 app = FastAPI()
 
@@ -10,7 +11,9 @@ async def receive_webhook(request: Request):
     payload = await request.json()
 
     print("--- Webhook Received ---")
-    print(payload)
+    print("Saving payload for inspection...")
+    with open("sample_payload.json", mode="w") as file:
+        json.dump(payload, file, indent=4)    
     print("----------------------")
 
     return {"status": "success", "message": "webhook received"}
